1315356b0bdbebed5ef7bbabe963038fe54adc47
Jeppe Reinhold <jeppereinhold@gmail.com>
2019-05-26 13:08:20 +0200

    updated node

--- node.sh
+++ node.sh
@@ -5,21 +5,28 @@
 # Common packages required for JavaScript development
 ########################################
 echo "----- WILL NOW INSTALL GLOBAL NPM PACKAGES -------"
-#install node.js with npm - JavaScript runner and package manager
+# install node.js with npm - JavaScript runner and package manager
 brew install node
 npm update -g npm
 brew install yarn
 
-#React Native CLI - to create and run React Native projects
-npm install -g react-native-cli
+# React Native CLI - to create and run React Native projects
+yarn global add react-native-cli
 
-#ESLint - To lint JavaScript files
-npm install -g eslint
+# ESLint - To lint JavaScript files
+yarn global add eslint
 
-#Serve - To create a minimalistisk http server local
-npm install -g serve
 
-#TypeScript compiler
-npm install -g typescript
+# ESLint - To lint JavaScript files
+yarn global add eslint
+
+# Prettier - format files
+yarn global add prettier
+
+# Serve - To create a minimalistisk http server local
+yarn global add serve
+
+# TypeScript compiler
+yarn global add typescript
 
 echo "----- NPM PACKAGES DONE-------"
