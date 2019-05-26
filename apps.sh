9a2aa47c522662adce2fcb55f077faab6cab301d
Jeppe Reinhold <jeppereinhold@gmail.com>
2019-05-26 13:05:07 +0200

    update apps

--- apps.sh
+++ apps.sh
@@ -18,119 +18,112 @@ caski () {
 ########################################
 echo "----- WILL NOW INSTALL APPS -------"
 
-#1Password - password manager
+# 1Password - password manager
 caski 1password
 
-#Google Chrome Browser
+# Browsers
 caski google-chrome
+caski brave-browser
+caski firefox
 
-#Fira Code Font - Monospace font with ligatures
+# Fira Code Font - Monospace font with ligatures
 brew tap caskroom/fonts
 caski font-fira-code
 
-#Visual Studio Code - Lightweight editor
+# Visual Studio Code - Lightweight editor
 caski visual-studio-code
-./vscode.sh #setup settings and extensions on first launch
+./vscode.sh # setup settings and extensions on first launch
 
-#Alfred - Launcher - NEEDS LICENCE
+# Alfred - Launcher - NEEDS LICENCE
 caski alfred
 
-#BetterTouchTool - tool to customize mouse and keyboard
+# BetterTouchTool - tool to customize mouse and keyboard
 caski bettertouchtool
 
-#HyperDock - Manage Dock and windows - NEEDS LICENSE
+# HyperDock - Manage Dock and windows - NEEDS LICENSE
 caski hyperdock
 
-#iStat Menus - Hardware Monitor - NEEDS LICENSE
+# iStat Menus - Hardware Monitor - NEEDS LICENSE
 caski istat-menus
 
-#Spectacle - Better window resizer and mover
+# Spectacle - Better window resizer and mover
 caski spectacle
 
-#Contexts - Better CMD+TAB
+# Contexts - Better CMD+TAB
 caski contexts
 
-#Wavebox - Single app for multiple web accounts, Inbox, Google Drive, Slack, etc.
-caski wavebox
-
-#Caprine - Facebook Messenger app
+# Caprine - Facebook Messenger app
 caski caprine
 
-#Slack - Messaging app
+# Slack - Messaging app
 caski slack
 
-#Screenhero - Screensharing app
-caski screenhero
-
-#Skype - voice communication app
+# Skype - voice communication app
 caski skype
 
-#Toggl - Timer app
+# Toggl - Timer app
 caski toggldesktop
 
-#Microsoft Office 2016
-#caski microsoft-office
-
-#Figma - Webbased vector app
+# Figma - Webbased vector app
 caski figma
 
-#Spotify - Music streaming app
+# Spotify - Music streaming app
 caski spotify
 
-#SourceTree - Git GUI
+# SourceTree - Git GUI
 caski sourcetree
 
-#Postman - API-tester
+# Postman - API-tester
 caski postman
 
-#iTerm2 - terminal
+# iTerm2 - terminal
 caski iterm2
 cp ./com.googlecode.iterm2.plist ~/Library/Preferences/
 
-#JDK - Java Development Kit, needed for Android SDK
+# JDK - Java Development Kit, needed for Android SDK
 caski java
 
-#Android Studio
+# Android Studio
 caski android-studio
 export ANDROID_HOME=${HOME}/Library/Android/sdk
 export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 
-#Arduino IDE
+# Arduino IDE
 caski arduino
 
-#Googles awesome screensaver - needs manual enabling in Sys Prefs
+# Fluor - enable/disable fn keys for specific apps
+caski fluor
+
+# Googles awesome screensaver - needs manual enabling in Sys Prefs
 caski google-featured-photos
 
-#Transmission - torrent downloader
+# Transmission - torrent downloader
 caski transmission
 
-#VLC - media player
+# VLC - media player
 caski vlc
 
-#Framer.js - prototyping app
-#caski framer
+# Teensy - Flashing tool for teensy boards (ErogDox keyboard)
+caski teensy
 
-#Keka - Archive manager (zip, rar, dmg, etc.)
+# Keka - Archive manager (zip, rar, dmg, etc.)
 caski keka
 
-#Handbrake - media encoder
+# Handbrake - media encoder
 caski handbrake
 
-#Docker - VM and container handler
+#  Videostream - Cast any video to Chromecast
+caski Videostream
+
+# Docker - VM and container handler
 caski docker
 
-#Android File Transfer - transfer files to a cabled android device
+# Android File Transfer - transfer files to a cabled android device
 caski android-file-transfer
 
-#NoSleep - System Utility to toggle wether mac sleeps
+# NoSleep - System Utility to toggle wether mac sleeps
 caski nosleep
-
-#DropBox - Cloud storage app
-#caski dropbox
-
-#Google Drive - Cloud storage app
-#caski google-drive
 
 ########################################
 # MAC APP STORE INSTALLS
